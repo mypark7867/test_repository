c55a17c7b452fd44ce392ec6a01703d5f1bbec5e
mypark7867 <mypark7867@gmail.com>
2016-05-30 10:27:54 +0900

    한국어 지원파일 추가(strings.xml(ko)) 및 삭제(strings.xml(kor)) 어플이름 수정

--- app/src/main/java/com/example/mypar/testrepository/MainActivity.java
+++ app/src/main/java/com/example/mypar/testrepository/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mypar.testrepository;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "" + "약속을 지키지 않는 사람을 저녁없어요.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
