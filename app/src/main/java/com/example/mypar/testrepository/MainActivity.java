bfac88a9b46f505dfb60e16931673862d302c3de
mypark7867 <mypark7867@gmail.com>
2016-05-30 15:19:45 +0900

    현재날짜시간 표현 및 EditText 문자열 가져오기

--- app/src/main/java/com/example/mypar/testrepository/MainActivity.java
+++ app/src/main/java/com/example/mypar/testrepository/MainActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         View button = (Button) findViewById(R.id.button);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "" + "약속을 지키지 않는 사람을 저녁없어요.", Toast.LENGTH_LONG).show();
+                EditText editText = (EditText) findViewById(R.id.editText);
+                //SimpleDateFormat format = new SimpleDateFormat("yyyy년 MM월 dd일", Locale.KOREA);
+                String nameString =
+                        new SimpleDateFormat("yyyy년 MM월 dd일", Locale.KOREA).format(new Date()) +
+                        " " + editText.getText().toString();
+
+                Toast.makeText(getApplicationContext(), nameString, Toast.LENGTH_LONG).show();
             }
         });
     }
